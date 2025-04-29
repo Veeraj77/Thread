db6a2aa26d86c8e5845f48c27b0da0768904b8ab
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:15:42 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+//Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Pokémon Battle Simulator ===");
+
+        String[] pikachuMoves = {"Thunderbolt", "Wild Charge", "Iron Tail", "Quick Attack"};
+        int[] pikachuDamage = {8, 10, 6, 4};
+
+        String[] snivyMoves = {"Leaf Storm", "Vine Whip", "Tackle", "Magical Leaf"};
+        int[] snivyDamage = {10, 6, 5, 7};
+
+        Pokemon pikachu = new Pokemon("Pikachu", "Electric", pikachuMoves, pikachuDamage);
+        Pokemon snivy = new Pokemon("Snivy", "Grass", snivyMoves, snivyDamage);
+
+        PokemonThread pikachuThread = new PokemonThread(pikachu);
+        PokemonThread snivyThread = new PokemonThread(snivy);
+
+        pikachuThread.setOpponent(snivy);
+        snivyThread.setOpponent(pikachu);
+
+        Thread t1 = new Thread(pikachuThread);
+        Thread t2 = new Thread(snivyThread);
+
+        t1.start();
+        t2.start();
