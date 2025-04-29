5892fbfe10306dad73f97ab414d3445e3dda9203
Veeraj77 <Veerajsahu07@gmail.com>
2025-04-30 01:17:27 +0530

    run and performround

--- PokemonThread.java
+++ PokemonThread.java
@@ -12,3 +12,37 @@ public class PokemonThread implements Runnable {
         this.opponent = opponent;
     }
 
+    @Override
+    public void run() {
+        System.out.println(pokemon.getName() + " (" + pokemon.getType() + " type) enters the battle!");
+
+        for (int round = 1; round <= 10; round++) {
+            if (pokemon.getHp() == 0 || opponent.getHp() == 0) {
+                break;
+            }
+
+            performRound();
+        }
+
+        // Final HP summary
+        System.out.println("\n== Battle Summary for " + pokemon.getName() + " ==");
+        System.out.println(pokemon.getName() + " Final HP: " + pokemon.getHp());
+    }
+
+    private void performRound() {
+        try {
+            pokemon.attack(opponent);
+            Thread.sleep(400);
+
+            if (opponent.getHp() > 0) {
+                pokemon.defend();
+                Thread.sleep(300);
+
+                pokemon.run();
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(pokemon.getName() + "'s round was interrupted.");
+        }
+    }
+}
